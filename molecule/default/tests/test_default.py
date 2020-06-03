9996855fd464552b41f336a655c51bbdf7373d99
Anthonius Munthi <me@itstoni.com>
2020-06-03 15:58:25 +0800

    test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -13,3 +13,8 @@ def test_hosts_file(host):
 def test_regolith(host):
 	p = host.package("regolith-desktop")
 	assert p.is_installed
+
+
+def test_gpg(host):
+	o = host.run("gpg --list-keys FC745865826D935D52E089583AF2DB499DD9D2A0")
+	assert o.succeeded
