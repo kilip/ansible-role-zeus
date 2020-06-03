6b21915800dc1be3fc6197134ddbc3e9e150cace
Anthonius Munthi <me@itstoni.com>
2020-06-03 11:51:21 +0800

    foo

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,9 +2,14 @@
 
 
 def test_hosts_file(host):
-    """Validate /etc/hosts file."""
-    f = host.file('/etc/hosts')
+	"""Validate /etc/hosts file."""
+	f = host.file('/etc/hosts')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+	assert f.exists
+	assert f.user == 'root'
+	assert f.group == 'root'
+
+
+def test_regolith(host):
+	p = host.package("regolith-desktop")
+	assert p.is_installed
