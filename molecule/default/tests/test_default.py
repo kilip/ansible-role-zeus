13090e325edec23523f12a5bdc459a38591a6183
Anthonius Munthi <me@itstoni.com>
2020-06-03 19:46:45 +0800

    wip
    * fixed tests
    * added gpg import
    * added php install

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,9 +2,19 @@
 
 
 def test_hosts_file(host):
-    """Validate /etc/hosts file."""
-    f = host.file('/etc/hosts')
+	"""Validate /etc/hosts file."""
+	f = host.file('/etc/hosts')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+	assert f.exists
+	assert f.user == 'root'
+	assert f.group == 'root'
+
+
+def test_regolith(host):
+	p = host.package("regolith-desktop")
+	assert p.is_installed
+
+
+def test_gpg(host):
+	o = host.run("gpg --list-keys FC745865826D935D52E089583AF2DB499DD9D2A0")
+	assert o.succeeded
