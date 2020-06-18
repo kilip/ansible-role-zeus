13b6f5072d357ed8c0e605eeecce77a8949b23d4
Anthonius Munthi <me@itstoni.com>
2020-06-18 13:36:08 +0800

    added rvm tools

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -11,6 +11,40 @@ def test_docker(host):
 	assert p.is_installed
 
 
+def test_git(host):
+	f = host.file("/home/toni/.gitconfig")
+	p = host.package("git")
+
+	assert f.is_file
+	assert f.contains("User Name")
+	assert p.is_installed
+
+
+def test_gpg(host):
+	o = host.run("runuser -l toni -c \"gpg --list-keys FC745865826D935D52E089583AF2DB499DD9D2A0\"")
+	assert o.succeeded
+
+
+def test_nvm(host):
+	assert host.file('/home/toni/.nvm').is_directory
+
+
+def test_phpbrew(host):
+	cmd = host.run("runuser -l toni -c 'phpbrew --version'")
+
+	assert cmd.succeeded
+
+
+def test_regolith(host):
+	p = host.package("regolith-desktop")
+	assert p.is_installed
+
+
+def test_rvm(host):
+	cmd = host.run("runuser -l toni -c 'rvm --version'")
+	assert cmd.succeeded
+
+
 def test_util(host):
 	v = host.package("vim")
 	n = host.package("nethogs")
@@ -21,38 +55,7 @@ def test_util(host):
 	assert c.is_installed
 
 
-def test_git(host):
-	f = host.file("/home/toni/.gitconfig")
-	p = host.package("git")
-
-	assert f.is_file
-	assert f.contains("User Name")
-	assert p.is_installed
-
-
-def test_regolith(host):
-	p = host.package("regolith-desktop")
-	assert p.is_installed
-
-
-def test_gpg(host):
-	o = host.run("su toni -c \"gpg --list-keys FC745865826D935D52E089583AF2DB499DD9D2A0\"")
-	assert o.succeeded
-
-
 def test_virtualbox(host):
 	v = host.package("virtualbox")
 
 	assert v.is_installed
-
-
-def test_phpbrew(host):
-	cmd = host.run("phpbrew --version")
-
-	assert cmd.succeeded
-
-
-def test_nvm(host):
-	cmd = host.run("su toni -c nvm --version")
-
-	assert cmd.succeeded
